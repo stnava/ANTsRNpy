f1d3b6e9898cd0cf653e7d4b6a73fbfea5cced01
stnava <stnava@gmail.com>
2016-12-21 10:40:14 -0500

    WIP: basic package via git

--- R/translationCode.R
+++ R/translationCode.R
@@ -1,5 +1,18 @@
-
-## a placeholder
-hello <- function(txt = "world") {
-    cat("Hello, ", txt, "\n")
+#' Simple whitening function.
+#'
+#' Whitens the input matrix using SVD and returns the result.
+#'
+#' @param x input matrix
+#' @param k rank to use
+#' @param reducex reduce the input matrix to k-size subspace
+#' @return matrix is output
+#' @author Avants BB
+#' @examples
+#'
+#' mat <- matrix(rnorm(300),ncol=50)
+#' wmat<-whiten( mat )
+#'
+#' @export whiten
+whiten <- function( x ) {
+  return( NA )
 }
