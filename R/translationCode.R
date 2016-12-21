2496ef5607e3f773c6e51e50bb612eb9e9bf2d37
stnava <stnava@gmail.com>
2016-12-21 12:39:11 -0500

    ENH: document this mapping

--- R/translationCode.R
+++ R/translationCode.R
@@ -14,8 +14,28 @@
 #' img = ANTsR::antsImageRead( ANTsR::getANTsRData( "r16" ) )
 #' ofn = tempfile( fileext=".npy" )
 #' writeANTsImageToNumpy( img, ofn )
-#' img = ANTsR::makeImage( c( 5, 6, 7 ) )
+#' img = ANTsR::makeImage( c( 5, 7 ), rnorm( 35 ) )
 #' writeANTsImageToNumpy( img, ofn )
+#' # to read in python, do
+#' # from PIL import Image
+#' # import numpy as np
+#' # from scipy.misc import toimage
+#' # data = np.load( ofn )
+#' # array = np.reshape( data, [ 7,5] )
+#' # toimage(array).show()
+#' img = ANTsR::makeImage( c( 5, 6, 7 ), rnorm( 5*6*7) )
+#' writeANTsImageToNumpy( img, ofn )
+#' # to read in python, do
+#' # from PIL import Image
+#' # import numpy as np
+#' # from scipy.misc import toimage
+#' # data = np.load( ofn )
+#' # array = np.reshape( data, [ 7, 6, 5 ] )
+#' # then we have
+#' # array[ 3, 1, 4 ] # index in python
+#' # as.array( img )[ 5, 2, 4 ] # index in R
+#' # so the python indices are in reverse order and minus one compared to R
+#' # toimage(array[:,:,2]).show()
 #'
 #' # to read in python, do
 #' # from PIL import Image
