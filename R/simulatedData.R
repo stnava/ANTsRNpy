b1f772780d7f8a84fcb86bc3e022803b72bd7c8f
stnava <stnava@gmail.com>
2017-01-12 17:06:29 -0500

    ENH: make a more challenging class problem

--- R/simulatedData.R
+++ R/simulatedData.R
@@ -8,6 +8,7 @@
 #' @param noiseLevel a two vector defining the noise level
 #' @param positionNoiseLevel a two vector defining the noise level controlling
 #' how much the image moves randomly away from the center.
+#' @param classByPosition boolean altering label by the sign of position change
 #' @return list containing the single channel image, the multi-channel image,
 #' the ground truth labels and the center of mass stored in a data frame.
 #' @author Avants BB
@@ -21,7 +22,8 @@
 simulateSphereData <- function( referenceImage,
   radius = c( 5, 6 ),
   noiseLevel = c( 0, 0.1 ),
-  positionNoiseLevel = c( 0, 2 ) )
+  positionNoiseLevel = c( 0, 2 ),
+  classByPosition = FALSE )
 {
   mydim = referenceImage@dimension
   idim = dim( referenceImage )
@@ -38,11 +40,13 @@ simulateSphereData <- function( referenceImage,
   labels = data.frame( labels = rep( 0, sum( msk ) ) )
   pts = matrix( nrow = 1, ncol = mydim )
   ctr = idim / 2
-  pts[1, ] = antsTransformIndexToPhysicalPoint( referenceImage, ctr ) +
-        rnorm( mydim, positionNoiseLevel[1], positionNoiseLevel[2] )
+  posNoise = rnorm( mydim, positionNoiseLevel[1], positionNoiseLevel[2] )
+  pts[1, ] = antsTransformIndexToPhysicalPoint( referenceImage, ctr ) + posNoise
   ptsi = makePointsImage( pts, msk, radius = baserad )
   ptsi = ptsi + makePointsImage( pts, msk, radius = plusrad )
   labels$labels = ptsi[ msk == 1 ]
+  if ( classByPosition & pts[ 1, 1 ] < 0 )
+    labels$labels = ptsi[ msk == 1 ] + max( labels$labels ) + 1
   mynoise = rnorm( sum( msk == 1 ), noiseLevel[1], noiseLevel[2] )
   mycom = getCenterOfMass( ptsi )
   ptsi[ msk == 1 ] = ptsi[ msk == 1 ] + mynoise
